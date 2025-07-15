8c71f3e46b65ac5b7eeabe0b5f82e026898f950e
SHgzg <1754737900@qq.com>
2025-07-15 23:12:57 +0800

    del

--- src/utils/withCache.js
+++
@@ -1,7 +0,0 @@
-const cacheMap = new Map()
-export const withCache = (fn)=>{
-    const cache =  cacheMap.get(fn)
-    if (cache) return cache
-    cacheMap.set(fn,fn())
-    return  cacheMap.get(fn)
-}
