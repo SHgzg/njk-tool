4bbda2c3d95183597e072df00cc5a10ca887733d
SHgzg <1754737900@qq.com>
2025-07-15 18:05:30 +0800

    0715

--- src/dev/index.js
+++ src/dev/index.js
@@ -31,19 +31,18 @@ function dividPath(filePath) {
 // const parts = text.split(/(Depends\[.*?\])/);
 class CompTool {
     constructor(path) {
-        this.getComp(path)
         const relativePath = getRelativePath(path)
         const { type, componentName } = dividPath(relativePath)
         this.path = path
         this.type = type
         this.name = componentName
         this.regex = this.genRegx(componentName)
-        const { deps, compStr } = this.getComp(path)
+        const { deps, compStr } = this.init(path)
         this.deps = deps
         this.compStr = compStr
     }
 
-    static isComp(path) {
+    static is(path) {
         return path.endsWith(".njk")
     }
 
@@ -52,7 +51,7 @@ class CompTool {
         return new RegExp(regexStr, "s")
     }
 
-    getComp(path) {
+    init(path) {
         const res = { deps: '', compStr: '' }
         try {
             res.compStr = readFileSync(path, "utf-8")
@@ -64,7 +63,7 @@ class CompTool {
         }
     }
 
-    wrapperComp() {
+    wrapper() {
         return `<!-- ${this.name} -->\n${this.compStr}\n<!-- /${this.name} -->\n`
     }
 
@@ -72,17 +71,17 @@ class CompTool {
         return target.template.match(this.regex)
     }
 
-    updateComp(target) {
-        target.setCompont(this.name, this.wrapperComp())
+    update(target) {
+        target.setCompont(this.name, this.wrapper())
     }
 
-    delateComp(target) {
-        target.setCompont(this.name, this.wrapperComp())
+    delate(target) {
+        target.setCompont(this.name, "")
 
     }
 
-    createComp(target) {
-        target.setCompont(this.name, this.wrapperComp())
+    create(target) {
+        target.setCompont(this.name, this.wrapper())
 
     }
 
@@ -93,23 +92,113 @@ class CompTool {
             const path = deps[index];
             const component = new CompTool(path)
             if (!component.isMatch(template)) {
-                component.createComp(target)
+                component.create(target)
             }
         }
     }
 }
 
-class Template {
+class StyleTool {
+    constructor(path) {
+        const relativePath = getRelativePath(path)
+        const { type, componentName } = dividPath(relativePath)
+        this.path = path
+        this.type = type
+        this.name = componentName
+        this.regex = this.genRegx(componentName)
+        const { deps, styleStr } = this.init(path)
+        this.deps = deps
+        this.styleStr = styleStr
+    }
+
+    static is(path) {
+        return path.endsWith(".css")
+    }
+
+    genRegx(name) { 
+        const regexStr = `\/\* ${name} \*\/([\s\S]*?)\/\* END-${name} \*\/`
+        return new RegExp(regexStr, "s")
+    }
+
+    init(path) {
+        const res = { deps: '', styleStr: '' }
+        try {
+            res.styleStr = readFileSync(path, "utf-8")
+        } catch (error) {
+            console.log(error);
+        } finally {
+
+            return res
+        }
+    }
+
+    wrapper() {
+        return `/* ${this.name} */\n${this.styleStr}\n/* END-${this.name} */\n`
+    }
+
+    isMatch(target) {
+        return target.template.match(this.regex)
+    }
+
+    update(target) {
+        target.setStyle(this.name, this.wrapper())
+    }
+
+    delate(target) {
+        target.setStyle(this.name, "")
+
+    }
+
+    create(target) {
+        target.setStyle(this.name, this.wrapper())
+
+    }
+}
+
+export class Template {
     constructor() {
-        this.root = `${process.cwd()}/src/templates/template.html`
+        this.template_root = `${process.cwd()}/src/templates/template.html`
+        this.style_root = `${process.cwd()}/src/templates/base.style`
         this.template = this.getTemplate()
+        this.style = this.getStyle()
         this.componentsMap = new Map()
-        this.sortQueue = ["Text.njk", "Title.njk", "Table.njk", "SideBar.njk", "Component.njk"]
+        this.styleMap = new Map()
+        this.sortComp = [
+            "Text.njk",
+            "Title.njk",
+            "Notice.njk",
+            "Block.njk",
+            "Date.njk",
+            "Table.njk",
+            "SideBar.njk",
+            "Tab.njk",
+            "Component.njk",
+        ]
+        this.sortCss = [
+            "Base.css",
+            "Text.css",
+            "Title.css",
+            "Notice.css",
+            "Block.css",
+            "Date.css",
+            "Table.css",
+            "SideBar.css",
+            "Tab.css",
+            "Component.css",
+        ]
     }
 
     getTemplate() {
         try {
-            return readFileSync(this.root, "utf-8")
+            return readFileSync(this.template_root, "utf-8")
+        } catch (error) {
+            console.log(error);
+            return ""
+        }
+    }
+    getStyle() {
+        try {
+            return readFileSync(this.style_root, "utf-8")
         } catch (error) {
             console.log(error);
             return ""
@@ -120,52 +209,99 @@ class Template {
         this.componentsMap.set(name, str)
     }
 
-    complierTemplate() {
-        const getNum =([k,v])=>this.sortQueue.findIndex(v=>v===k)
+    setStyle(name, str) {
+        this.styleMap.set(name, str)
+    }
+
+    complierComponent() {
+        const getNum = ([k, v]) => this.sortComp.findIndex(v => v === k) || 999
         const res = Array.from(this.componentsMap)
-        .sort((a,b)=>{
-            return getNum(a)-getNum(b)
-        })
-        .map(([k,v])=>v)
-        .join("\n")
+            .sort((a, b) => {
+                return getNum(a) - getNum(b)
+            })
+            .map(([k, v]) => v)
+            .join("\n")
         return res
     }
 
-    buildTemplate() {
-        writeFileSync(this.root, this.complierTemplate(), "utf-8")
+    complierStyle() {
+        const getNum = ([k, v]) => this.sortCss.findIndex(v => v === k) || 999
+        const res = Array.from(this.styleMap)
+            .sort((a, b) => {
+                return getNum(a) - getNum(b)
+            })
+            .map(([k, v]) => {
+                return v
+            })
+            .join("\n")
+        return res
+    }
+
+    buildComponent() {
+        writeFileSync(this.template_root, this.complierComponent(), "utf-8")
+    }
+
+    buildStyle() {
+        writeFileSync(this.style_root, this.complierStyle(), "utf-8")
     }
 }
 
 const template = new Template()
 
-export function addHandler(path) {
-    if (!CompTool.isComp(path)) return
-    const component = new CompTool(path)
-    if (component.isMatch(template)) {
-        component.updateComp(template)
-    } else {
-        component.createComp(template)
+const FactoryMap = {
+    Style: StyleTool,
+    Component: CompTool,
+}
+
+function HandlerFactory(type) {
+    const Factory = FactoryMap[type]
+    return function (evnt, path) {
+        if (!Factory.is(path)) return
+        const instance = new Factory(path)
+        switch (evnt) {
+            case 'add':
+                instance.isMatch(template) ? instance.update(template)
+                    : instance.create(template)
+                template?.[`build${type}`]()
+                break;
+            case 'change':
+                if (!instance.isMatch(template)) return
+                instance.update(template)
+                template?.[`build${type}`]()
+                break;
+            case 'unlink':
+                if (!instance.isMatch(template)) return
+                instance.delate(template)
+                template?.[`build${type}`]()
+                break;
+        }
     }
-    template.buildTemplate()
-    return
+}
+
+const compHandler = HandlerFactory("Component")
+const styleHandler = HandlerFactory("Style")
+
+
+export function addHandler(path) {
+    if (CompTool.is(path)) {
+        return compHandler("add", path)
+    } else if (StyleTool.is(path)) {
+        return styleHandler("add", path)
+    }
 }
 
 export function changeHandler(path) {
-    if (!CompTool.isComp(path)) return
-    const component = new CompTool(path)
-    if (component.isMatch(template)) {
-        component.updateComp(template)
+    if (CompTool.is(path)) {
+        return compHandler("change", path)
+    } else if (StyleTool.is(path)) {
+        return styleHandler("change", path)
     }
-    template.buildTemplate()
-    return
 }
 
 export function unlinkHandler(path) {
-    if (!CompTool.isComp(path)) return
-    const component = new CompTool(path)
-    if (component.isMatch(template)) {
-        component.delateComp(template)
+    if (CompTool.is(path)) {
+        return compHandler("unlink", path)
+    } else if (StyleTool.is(path)) {
+        return styleHandler("unlink", path)
     }
-    template.buildTemplate()
-    return
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 
 
 import express from "express"
-import { readFileSync } from "fs";
+import { readFileSync, writeFileSync } from "fs";
 import nunjucks from "nunjucks";
 import { join } from "path";
 import { addHandler, changeHandler, unlinkHandler, watcher } from "./dev/index.js";
@@ -19,10 +19,10 @@ nunjucks.configure(externalTemplatesDir, {
 // 监听事件
 watcher
     .on('add', addHandler)
-    .on('change',changeHandler)
+    .on('change', changeHandler)
     .on('unlink', unlinkHandler)
-    // .on('addDir', console.log(`新建目录: ${path}`))
-    // .on('unlinkDir', path => console.log(`删除目录: ${path}`));
+// .on('addDir', console.log(`新建目录: ${path}`))
+// .on('unlinkDir', path => console.log(`删除目录: ${path}`));
 
 const port = 3007
 app.listen(port, () => {
@@ -30,16 +30,27 @@ app.listen(port, () => {
 });
 
 app.get('/example/:file', (req, res) => {
-     const { file } = req.params
-    const mockData = useMockData({path:`${process.cwd()}/src/examples/${file}.json`})
+    const { file } = req.params
+    const mockData = useMockData({ path: `${process.cwd()}/src/examples/${file}.json` })
     const html = nunjucks.render(`./templates/template.html`, { ctx: mockData })
     const css = getBaseCss()
     res.send(html + css)
 })
 
-function getBaseCss(){
-     const css = readFileSync(join(`${process.cwd()}/src/templates`, `base.css`), 'utf8')
-    return  `<style>${css}</style>`
+function getBaseCss() {
+    const css = readFileSync(join(`${process.cwd()}/src/templates`, `base.style`), 'utf8')
+    return `<style>${css}</style>`
+}
+
+build()
+
+function build() {
+    const writePath = `${process.cwd()}/dist/template.html`
+    const css = readFileSync(join(`${process.cwd()}/src/templates`, `base.style`), 'utf8')
+    const template = readFileSync(join(`${process.cwd()}/src/templates`, `template.html`), 'utf8')
+    const res = `<style>${css}</style>` + template
+    writeFileSync(writePath, res, "utf-8")
+
 }
 
 
@@ -54,5 +65,5 @@ function getBaseCss(){
 // http://localhost:3005/comp/title
 
 // windows
-//  netstat -ano | findstr ":3005"  
+//  netstat -ano | findstr ":3005"
 // Stop-Process -Id 33108 -Force
