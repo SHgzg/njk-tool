d380e292aa69912a930d62a1258e6e16e390222c
SHgzg <1754737900@qq.com>
2025-07-17 18:09:54 +0800

    1

--- rollup.config.js
+++ rollup.config.js
@@ -6,47 +6,41 @@ import json from '@rollup/plugin-json';
 import copy from 'rollup-plugin-copy'
 import { defineConfig } from 'rollup';
 
-const commonConfig = {
-    // 项目入口
-    input: 'src/index.ts',
-    plugins: [
-        typescript({
-            tsconfig: './tsconfig.json', // 指定配置文件路径
-            sourceMap: true,             // 生成 Source Map
-            inlineSources: true,         // 内联源码到 Source Map
-        }),
-        json(),
-        resolve(),
-        commonjs(),
-        copy({
-            targets: [
-                { src: 'src/index.html', dest: 'dist/bundle/src' },
-                { src: 'src/base.style', dest: 'dist/bundle/src' },
-            ]
-        })
-    ],
-}
+const plugins = [
+    typescript({
+        tsconfig: './tsconfig.json', // 指定配置文件路径
+        sourceMap: true,             // 生成 Source Map
+        inlineSources: true,         // 内联源码到 Source Map
+    }),
+    json(),
+    resolve(),
+    commonjs(),
+    copy({
+        targets: [
+            { src: 'src/index.html', dest: 'dist/bundle/src' },
+            { src: 'src/base.style', dest: 'dist/bundle/src' },
+        ]
+    })
+]
 
 
- const config = defineConfig([
+const config = defineConfig(
     {
-        output: {
+        input: 'src/index.ts',
+        output: [{
             file: 'dist/bundle/app.min.js',
             format: 'esm',
             sourcemap: true,
             exports: 'named',
-        },
-        ...commonConfig,
-    },
-    {
-        output: {
+        }, {
             file: 'dist/bundle/app.min.cjs',
             format: 'cjs',
             sourcemap: true,
             exports: 'named',
-        },
-        ...commonConfig,
-    }
-])
+        }],
+        plugins,
+    },
+
+)
 
 export default config
